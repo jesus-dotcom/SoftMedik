d330dfc3cc95e8392a9d526918cf21c88f4a87fa
jesus-dotcom <61996045+jesus-dotcom@users.noreply.github.com>
2022-09-12 10:14:50 -0400

    Delete seguridad.php

--- seguridad.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//seguridad de sessiones paginacion
-session_start();
-error_reporting(0);
-$varsesion = $_SESSION['Nickname_Usuario'];
-if($varsesion == null || $varsesion ==''){
-  header('location: ../index.php');
-  die();
-}
