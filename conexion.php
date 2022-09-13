e79396fd577398f09a4d390aa5f71ddc28568079
jesus-dotcom <61996045+jesus-dotcom@users.noreply.github.com>
2022-09-13 16:22:14 -0400

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include("configuracion.php");
-$conexion = new mysqli($server,$user,$pass,$bd);
-if(mysqli_connect_errno()){
-	echo "No se hizo la conexion a la base de datos",mysql_connect_errno();
-	exit();
-}
